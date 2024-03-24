00cd1e5dcdd2b833a014148b54406ca38c1f0683
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2024-03-24 16:42:04 +0200

    Add files via upload

---
+++ materials.js
@@ -0,0 +1,38 @@
+const materials = {
+    "Weirwood": {
+        "img": "materials/weirwood.jpg"
+    },
+    "Kingswood oak": {
+        "img": "materials/kingswood-oak.jpg"
+    },
+    "Hide": {
+        "img": "materials/hide.jpg"
+    },
+    "Leather straps": {
+        "img": "materials/leather-straps.jpg"
+    },
+    "Milk of the poppy": {
+        "img": "materials/milk-of-the-poppy.jpg"
+    },
+    "Ironwood": {
+        "img": "materials/ironwood.jpg"
+    },
+    "Black Iron": {
+        "img": "materials/black-iron.jpg"
+    },
+    "Copper bar": {
+        "img": "materials/copper-bar.jpg"
+    },
+    "Dragonglass": {
+        "img": "materials/dragonglass.jpg"
+    },
+    "Goldenheart wood": {
+        "img": "materials/goldenheart-wood.jpg"
+    },
+    "Silk": {
+        "img": "materials/silk.jpg"
+    },
+    "Wildfire": {
+        "img": "materials/wildfire.jpg"
+    }
+};
