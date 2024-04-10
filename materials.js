b822479935306a498609d0a0067ebfd4c7b02df7
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2024-04-10 21:18:06 +0300

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,26 +1,8 @@
 const materials = {
-    "Weirwood": {
-        "img": "materials/weirwood.jpg"
-    },
-    "Kingswood oak": {
-        "img": "materials/kingswood-oak.jpg"
-    },
-    "Hide": {
-        "img": "materials/hide.jpg"
-    },
-    "Leather straps": {
-        "img": "materials/leather-straps.jpg"
-    },
-    "Milk of the poppy": {
-        "img": "materials/milk-of-the-poppy.jpg"
-    },
-    "Ironwood": {
-        "img": "materials/ironwood.jpg"
-    },
-    "Black Iron": {
+	"Black Iron": {
         "img": "materials/black-iron.jpg"
     },
-    "Copper bar": {
+	"Copper bar": {
         "img": "materials/copper-bar.jpg"
     },
     "Dragonglass": {
@@ -29,8 +11,26 @@ const materials = {
     "Goldenheart wood": {
         "img": "materials/goldenheart-wood.jpg"
     },
+    "Hide": {
+        "img": "materials/hide.jpg"
+    },
+    "Ironwood": {
+        "img": "materials/ironwood.jpg"
+    },
+    "Kingswood oak": {
+        "img": "materials/kingswood-oak.jpg"
+    },
+    "Leather straps": {
+        "img": "materials/leather-straps.jpg"
+    },
+    "Milk of the poppy": {
+        "img": "materials/milk-of-the-poppy.jpg"
+    },
     "Silk": {
         "img": "materials/silk.jpg"
+    },
+    "Weirwood": {
+        "img": "materials/weirwood.jpg"
     },
     "Wildfire": {
         "img": "materials/wildfire.jpg"
