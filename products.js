e79b102e2a78e162c22191c26e4cd31273e41bd2
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2024-10-18 16:47:08 +0300

    Update products.js

--- products.js
+++ products.js
@@ -4,145 +4,145 @@ let craftItem = {
       "name": "Wooden Bucket",
       "level": 1,
       "materials": {"Weirwood": 10240, "Kingswood oak": 10240},
-      "img": "item/helmet.png"
+      "img": "item/icon_eq_standard_helmet_woodenbucket.webp"
     },
     {
       "name": "Leather Vest",
       "level": 1,
       "materials": {"Hide": 10240, "Leather straps": 10240},
-      "img": "item/plate.png"
+      "img": "item/icon_eq_standard_chest_leathervest.webp"
     },
     {
       "name": "Flax Legwraps",
       "level": 1,
       "materials": {"Milk of the poppy": 10240, "Goldenheart wood": 10240},
-      "img": "item/pants.png"
+      "img": "item/icon_eq_standard_pants_flaxlegwraps.webp"
     },
 	{
       "name": "Straw Sanals",
       "level": 1,
       "materials": {"Hide": 10240, "Leather straps": 10240},
-      "img": "item/shoos.png"
+      "img": "item/icon_eq_standard_boots_strawsandals.webp"
     },
     {
       "name": "Copper Band",
       "level": 1,
       "materials": {"Black Iron": 10240, "Copper bar": 10240},
-      "img": "item/ring.png"
+      "img": "item/icon_eq_standard_ring_copperband.webp"
     },
     {
       "name": "Dagger",
       "level": 1,
       "materials": {"Ironwood": 10240, "Kingswood oak": 10240},
-      "img": "item/dagger.png"
+      "img": "item/icon_eq_standard_weapon_shortsword.webp"
     },
     {
       "name": "Wool Bandana",
       "level": 5,
       "materials": {"Kingswood oak": 15360, "Milk of the poppy": 15360},
-      "img": "item/helmet.png"
+      "img": "item/icon_eq_standard_helmet_woolbandana.webp"
     },
     {
       "name": "Boiled Leather Shirt",
       "level": 5,
       "materials": {"Hide": 15360, "Dragonglass": 15360},
-      "img": "item/plate.png"
+      "img": "item/icon_eq_standard_chest_boiledleathershirt"
     },
     {
       "name": "Leather Poleyns",
       "level": 5,
       "materials": {"Hide": 15360, "Leather straps": 15360},
-      "img": "item/pants.png"
+      "img": "item/icon_eq_standard_pants_leatherpoleyns.webp"
     },
     {
       "name": "Leather Booties",
       "level": 5,
       "materials": {"Hide": 10240, "Silk": 10240, "Weirwood": 10240},
-      "img": "item/shoos.png"
+      "img": "item/icon_eq_standard_boots_leatherbooties.webp"
     },
     {
       "name": "Bronze Band",
       "level": 5,
       "materials": {"Copper bar": 15360, "Milk of the poppy": 15360},
-      "img": "item/ring.png"
+      "img": "item/icon_eq_standard_ring_bronzeband.webp"
     },
     {
       "name": "Spear",
       "level": 5,
       "materials": {"Leather straps": 15360, "Wildfire": 15360},
-      "img": "item/dagger.png"
+      "img": "item/icon_eq_standard_weapon_spear.webp"
     },
     {
       "name": "Copper Pot",
       "level": 10,
       "materials": {"Copper bar": 30720, "Leather straps": 30720},
-      "img": "item/helmet.png"
+      "img": "item/icon_eq_standard_helmet_copperpot.webp"
     },
     {
       "name": "Straw hat",
       "level": 10,
       "materials": {"Dragonglass": 20480, "Silk": 20480, "Leather straps": 20480},
-      "img": "item/helmet.png"
+      "img": "item/icon_eq_standard_helmet_strawhat.webp"
     },
     {
       "name": "Wool Robe",
       "level": 10,
       "materials": {"Milk of the poppy": 30720, "Hide": 30720},
-      "img": "item/plate.png"
+      "img": "item/icon_eq_standard_chest_woolrobe.webp"
     },
     {
       "name": "Padded Armor",
       "level": 10,
       "materials": {"Silk": 30720, "Ironwood": 30720},
-      "img": "item/plate.png"
+      "img": "item/icon_eq_standard_chest_paddedarmor.webp"
     },
     {
       "name": "Wool Leggings",
       "level": 10,
       "materials": {"Hide": 30720, "Wildfire": 30720},
-      "img": "item/pants.png"
+      "img": "item/icon_eq_standard_pants_woolleggings.webp"
     },
     {
       "name": "Ragged Shorts",
       "level": 10,
       "materials": {"Leather straps": 30720, "Milk of the poppy": 30720},
-      "img": "item/pants.png"
+      "img": "item/icon_eq_standard_pants_raggedshorts.webp"
     },
     {
       "name": "Velvet Boots",
       "level": 10,
       "materials": {"Silk": 30720, "Wildfire": 30720},
-      "img": "item/shoos.png"
+      "img": "item/icon_eq_standard_boots_velvetboots.webp"
     },
     {
       "name": "Galoshes",
       "level": 10,
       "materials": {"Weirwood": 30720, "Ironwood": 30720},
-      "img": "item/shoos.png"
+      "img": "item/icon_eq_standard_boots_galoshes.webp"
     },
     {
       "name": "Pyrite Lament",
       "level": 10,
       "materials": {"Dragonglass": 30720, "Kingswood oak": 30720},
-      "img": "item/ring.png"
+      "img": "item/icon_eq_standard_ring_pyritelament.webp"
     },
     {
       "name": "Quartz Ring",
       "level": 10,
       "materials": {"Dragonglass": 30720, "Weirwood": 30720},
-      "img": "item/ring.png"
+      "img": "item/icon_eq_standard_ring_quartzring.webp"
     },
     {
       "name": "Mace",
       "level": 10,
       "materials": {"Weirwood": 30720, "Black Iron": 30720},
-      "img": "item/dagger.png"
+      "img": "item/icon_eq_standard_weapon_mace.webp"
     },
     {
       "name": "Short Bow",
       "level": 10,
       "materials": {"Silk": 30720, "Goldenheart wood": 30720},
-      "img": "item/dagger.png"
+      "img": "item/icon_eq_standard_weapon_shortbow.webp"
     }
   ]
 };
