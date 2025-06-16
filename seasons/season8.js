b133f55d4d367c3477c8eb4f6fff18124a2dad93
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-16 23:05:52 +0300

    Update season8.js

--- seasons/season8.js
+++ seasons/season8.js
@@ -319,5 +319,323 @@ const season8 = {
 			}
 		  ]
 		},
+		{
+		  "setName": "Dragon Rider's",
+		  "setMat": "Dragon Tempered Fabrics",
+		  "products": [
+			{
+			  "name": "Jeweled Adornment",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"wildfire": 10,
+				"dragonglass": 10
+			  },
+			  "img": "item/season8/dragon-rider/head.png"
+			},
+			{
+			  "name": "Jacket",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"kingswood-oak": 10,
+				"copper-bar": 10
+			  },
+			  "img": "item/season8/dragon-rider/chest.png"
+			},
+			{
+			  "name": "Bases",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"ironwood": 10,
+				"wildfire": 10
+			  },
+			  "img": "item/season8/dragon-rider/pants.png"
+			},
+			{
+			  "name": "Heeled Boots",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"milk-of-the-poppy": 10,
+				"kingswood-oak": 10
+			  },
+			  "img": "item/season8/dragon-rider/boots.png"
+			},
+			{
+			  "name": "Encrusted Ring",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"weirwood": 10,
+				"black-iron": 10
+			  },
+			  "img": "item/season8/dragon-rider/ring.png"
+			},
+			{
+			  "name": "Keen Spears",
+			  "level": 5,
+			  "materials": {
+				"dragon-tempered": 10,
+				"hide": 10,
+				"goldenheart-wood": 10
+			  },
+			  "img": "item/season8/dragon-rider/weapon.png"
+			},
+			{
+			  "name": "Jeweled Adornment",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"wildfire": 20,
+				"dragonglass": 20
+			  },
+			  "img": "item/season8/dragon-rider/head.png"
+			},
+			{
+			  "name": "Jacket",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"kingswood-oak": 20,
+				"copper-bar": 20
+			  },
+			  "img": "item/season8/dragon-rider/chest.png"
+			},
+			{
+			  "name": "Bases",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"ironwood": 20,
+				"wildfire": 20
+			  },
+			  "img": "item/season8/dragon-rider/pants.png"
+			},
+			{
+			  "name": "Heeled Boots",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"milk-of-the-poppy": 20,
+				"kingswood-oak": 20
+			  },
+			  "img": "item/season8/dragon-rider/boots.png"
+			},
+			{
+			  "name": "Encrusted Ring",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"weirwood": 20,
+				"black-iron": 20
+			  },
+			  "img": "item/season8/dragon-rider/ring.png"
+			},
+			{
+			  "name": "Keen Spears",
+			  "level": 10,
+			  "materials": {
+				"dragon-tempered": 20,
+				"hide": 20,
+				"goldenheart-wood": 20
+			  },
+			  "img": "item/season8/dragon-rider/weapon.png"
+			},
+			{
+			  "name": "Jeweled Adornment",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"wildfire": 120,
+				"dragonglass": 120
+			  },
+			  "img": "item/season8/dragon-rider/head.png"
+			},
+			{
+			  "name": "Jacket",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"kingswood-oak": 120,
+				"copper-bar": 120
+			  },
+			  "img": "item/season8/dragon-rider/chest.png"
+			},
+			{
+			  "name": "Bases",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"ironwood": 120,
+				"wildfire": 120
+			  },
+			  "img": "item/season8/dragon-rider/pants.png"
+			},
+			{
+			  "name": "Heeled Boots",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"milk-of-the-poppy": 120,
+				"kingswood-oak": 120
+			  },
+			  "img": "item/season8/dragon-rider/boots.png"
+			},
+			{
+			  "name": "Encrusted Ring",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"weirwood": 120,
+				"black-iron": 120
+			  },
+			  "img": "item/season8/dragon-rider/ring.png"
+			},
+			{
+			  "name": "Keen Spears",
+			  "level": 15,
+			  "materials": {
+				"dragon-tempered": 120,
+				"hide": 120,
+				"goldenheart-wood": 120
+			  },
+			  "img": "item/season8/dragon-rider/weapon.png"
+			},
+			{
+			  "name": "Jeweled Adornment",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"wildfire": 400,
+				"dragonglass": 400,
+				"milk-of-the-poppy": 400
+			  },
+			  "img": "item/season8/dragon-rider/head.png"
+			},
+			{
+			  "name": "Jacket",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"kingswood-oak": 400,
+				"copper-bar": 400,
+				"goldenheart-wood": 400
+			  },
+			  "img": "item/season8/dragon-rider/chest.png"
+			},
+			{
+			  "name": "Bases",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"ironwood": 400,
+				"wildfire": 400,
+				"copper-bar": 400
+			  },
+			  "img": "item/season8/dragon-rider/pants.png"
+			},
+			{
+			  "name": "Heeled Boots",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"milk-of-the-poppy": 400,
+				"kingswood-oak": 400,
+				"ironwood": 400
+			  },
+			  "img": "item/season8/dragon-rider/boots.png"
+			},
+			{
+			  "name": "Encrusted Ring",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"weirwood": 400,
+				"black-iron": 400,
+				"silk": 400
+			  },
+			  "img": "item/season8/dragon-rider/ring.png"
+			},
+			{
+			  "name": "Keen Spears",
+			  "level": 20,
+			  "materials": {
+				"dragon-tempered": 400,
+				"hide": 400,
+				"goldenheart-wood": 400,
+				"leather-straps": 400
+			  },
+			  "img": "item/season8/dragon-rider/weapon.png"
+			},
+			{
+			  "name": "Jeweled Adornment",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"wildfire": 1200,
+				"dragonglass": 1200,
+				"milk-of-the-poppy": 1200
+			  },
+			  "img": "item/season8/dragon-rider/head.png"
+			},
+			{
+			  "name": "Jacket",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"kingswood-oak": 1200,
+				"copper-bar": 1200,
+				"goldenheart-wood": 1200
+			  },
+			  "img": "item/season8/dragon-rider/chest.png"
+			},
+			{
+			  "name": "Bases",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"ironwood": 1200,
+				"wildfire": 1200,
+				"copper-bar": 1200
+			  },
+			  "img": "item/season8/dragon-rider/pants.png"
+			},
+			{
+			  "name": "Heeled Boots",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"milk-of-the-poppy": 1200,
+				"kingswood-oak": 1200,
+				"ironwood": 1200
+			  },
+			  "img": "item/season8/dragon-rider/boots.png"
+			},
+			{
+			  "name": "Encrusted Ring",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"weirwood": 1200,
+				"black-iron": 1200,
+				"silk": 1200
+			  },
+			  "img": "item/season8/dragon-rider/ring.png"
+			},
+			{
+			  "name": "Keen Spears",
+			  "level": 25,
+			  "materials": {
+				"dragon-tempered": 1200,
+				"hide": 1200,
+				"goldenheart-wood": 1200,
+				"leather-straps": 1200
+			  },
+			  "img": "item/season8/dragon-rider/weapon.png"
+			}
+		  ]
+		}
 	]
 };
